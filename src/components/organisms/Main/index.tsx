2e205be3e277e7ae0ddc07f6fce99ad1e44dc67d
Alexander Grishin <grishin057@gmail.com>
2021-03-07 16:53:57 +0300

    main button

--- src/components/organisms/Main/index.tsx
+++ src/components/organisms/Main/index.tsx
@@ -18,7 +18,7 @@ const Main: React.FC = () => {
           <Typography className={classes.title}>Пользователи сервиса обмениваются книгами</Typography>
           <Typography className={classes.subtitle}>Пользователь помечает категории, типы, тэги книг, которые бы он хотел получить и такие же характеристики для книг, которые он может отдать. Система подбирает пользователя для обмена и контролирует что обмен состоялся. Система выявляет недобросовестных пользователей и принимает соответствующие меры. </Typography>
           <Typography className={classes.subtitle}>Cервис позволит упростить процесс обмена книгами, сделает его более привлекательным и надежным, будет способствовать привлечению большего количества людей к обмену бумажными книгами.</Typography>
-          <ButtonItem type="disabled" className={classes.btn}> Начать обмен</ButtonItem>
+          <ButtonItem className={classes.btn}> Начать обмен</ButtonItem>
         </Box>
         <Box className={classes.imageBlock}>
           <img className={classes.image} src={main} alt="Main picture"/>
