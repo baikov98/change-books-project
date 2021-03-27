95da5eeafac8b0dd6e7419dd3a5e0d50dcdd59ad
Alexander Grishin <grishin057@gmail.com>
2021-03-27 22:09:03 +0300

    hot fix reviews

--- src/components/molecules/ReviewsItem/index.tsx
+++ src/components/molecules/ReviewsItem/index.tsx
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import { useStyles } from "./styles";
 import { Box, Typography } from "@material-ui/core";
 
-interface IProps {
-  data: any; // демонстративные данные
+interface IReview {
+  id: number;
+  user: string;
+  created_at: Date;
+  response: string;
 }
 
-const ReviewsItem: React.FC<IProps> = ({ data }: IProps) => {
+interface IProps{
+  data: IReview,
+}
+
+const ReviewsItem: React.FC<IProps> = ({data}: IProps) => {
+  const { id, user, created_at, response } = data
   const [readmore, setReadmore] = useState(true);
   const classes = useStyles();
   const readmoreButtonText = readmore ? "Читать полностью" : "Свернуть";
@@ -14,18 +22,17 @@ const ReviewsItem: React.FC<IProps> = ({ data }: IProps) => {
   const handleClick = () => {
     setReadmore(!readmore);
   };
-
   return (
     <Box className={classes.root}>
       <Typography className={readmore ? classes.topic : opened}>
-        {data?.username}
+        {user || 'UserName'}
       </Typography>
-      <Typography>{data?.date}</Typography>
+      <Typography>{ new Date(created_at).toLocaleDateString() || new Date().toLocaleDateString()}</Typography>
       <Box className={readmore ? classes.text : opened}>
         <Typography className={readmore ? classes.textMore : opened}>
-          {data?.text}
+          {response || "Текст отзыва"}
         </Typography>
-        {data?.text.length > 50 && (
+        {response?.length > 50 && (
           <Typography className={classes.button} onClick={handleClick}>
             {readmoreButtonText}
           </Typography>
